10418709436842ddb2362fb47591302ff9700fda
Nil Ordoñez <nilordonez.7@gmail.com>
2018-05-03 17:57:24 +0200

    Clase Spaceship implementada

---
+++ M08_UF3_Practica_BMerelo_NOrdonez/Models/Spaceship.swift
@@ -0,0 +1,60 @@
+//
+//  Spaceship.swift
+//  M08_UF3_Practica_BMerelo_NOrdonez
+//
+//  Created by Nil Ordoñez Romera on 3/5/18.
+//  Copyright © 2018 Nil Ordoñez. All rights reserved.
+//
+
+import UIKit
+
+class Spaceship: UIImageView {
+    
+    let speed: CGFloat = 1.5
+    var moveToPoint:CGPoint?
+    
+    override init(image: UIImage?) {
+        super.init(image: image)
+    }
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func updateLocation(){
+        if let newPoint = moveToPoint{
+            if !(newPoint.x == self.center.x) && !(newPoint.y==self.center.y) {
+                var center = self.center
+                
+                let dx:CGFloat = newPoint.x - center.x
+                
+                var dxf:CGFloat = self.speed
+                
+                dxf = dx < 0 ? dxf * -1 : dxf
+                
+                center.x = center.x + dxf
+                
+                if (abs(newPoint.x - center.x) < self.speed) {
+                    center.x = newPoint.x
+                }
+                
+                self.center = center
+            }
+        }
+    }
+    
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- M08_UF3_Practica_BMerelo_NOrdonez/ViewController.swift
+++ M08_UF3_Practica_BMerelo_NOrdonez/ViewController.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var spaceship: Spaceship?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.spaceship = Spaceship(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        self.spaceship!.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY)
+        self.spaceship!.moveToPoint = self.spaceship!.center
+        self.view.addSubview(self.spaceship!)
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGestureDone))
+        tapGesture.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tapGesture)
+        self.view.isUserInteractionEnabled = true
+        
+        let displayLink = CADisplayLink(target: self, selector: #selector(updateScene))
+        //displayLink.add(to: self.view.current, forMode: <#T##RunLoopMode#>)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    
+    @objc func tapGestureDone(){
+        
+    }
+    
+    @objc func updateScene(){
+        
     }
 
 
